d402b150dd008ea467ef4a9dc249a951e3fce05d
Mostafizur.Rahman@coxautoinc.com <Mostafizur.Rahman@coxautoinc.com>
2024-05-27 09:10:04 -0500

    added onClick event to show what link has been clicked

---
+++ index.js
@@ -0,0 +1,21 @@
+function clickOnHome() {
+    alert("You have clicked on Home link");
+}
+function clickOnNews() {
+    alert("You have clicked on News link");
+}
+function clickOnContact() {
+    alert("You have clicked on Contact link");
+}
+function clickOnAbout() {
+    alert("You have clicked on About link");
+}
+function clickOnProfile() {
+    alert("You have clicked on Profile link");
+}
+function clickOnServices() {
+    alert("You have clicked on Services link");
+}
+function clickOnClients() {
+    alert("You have clicked on Clients link");
+}
